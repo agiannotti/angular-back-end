663391ed84f27e0af5c2744ef0308de0aafb01e1
tonyg <aj.gianno@gmail.com>
2021-04-23 16:04:17 -0500

    http request format

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-posts',
+  selector: 'posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class PostsComponent {
+  posts: any;
+  constructor(http: HttpClient) {
+    http
+      .get('https://jsonplaceholder.cypress.io/todos/')
+      .subscribe((response) => {
+        this.posts = response;
+        console.log(response);
+      });
   }
-
 }
